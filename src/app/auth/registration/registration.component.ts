79ec0cee88c7ec57099fc4d4ad25ced2e9f296ee
Rodion <rodion.zhuvagin@sharp-dev.net>
2020-03-19 19:24:20 +0400

    Added simple form to registration component

--- src/app/auth/registration/registration.component.ts
+++ src/app/auth/registration/registration.component.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.scss']
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit, OnDestroy{
+  registrationForm: FormGroup;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.registrationForm = new FormGroup({
+      username: new FormControl(
+        null,
+        Validators.required
+      ),
+      password: new FormControl(
+        null,
+        [Validators.required, Validators.pattern(/[0-9A-Z]/)]
+      ),
+      email: new FormControl(
+        null,
+        [Validators.required, Validators.email]
+      ),
+    });
+    this.registrationForm.statusChanges.subscribe( status => console.log(this.registrationForm))
+  }
+
+  ngOnDestroy() {
+
+  }
 }
